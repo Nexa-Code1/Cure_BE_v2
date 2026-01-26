572869a4890657afb19e7e6fe7c5c92cd319f4ff
HagarRagab <101262480+HagarRagab@users.noreply.github.com>
2026-01-26 16:59:38 +0200

    add globalErrorHandler Middleware

--- src/Modules/Specialists/specialists.controller.js
+++ src/Modules/Specialists/specialists.controller.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { authenticationMiddleware } from "../../Middlewares/authentication-middleware.js";
 import { getSpecialists } from "./Services/specialists.service.js";
+import { errorHandlerMiddleware } from "../../Middlewares/error-handler-middleware.js";
 
 const specialistsRouter = Router();
 
